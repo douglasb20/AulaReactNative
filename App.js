5acbfaa3b8e9d14965a2808ea50845ba1c0021c5
Douglas Amaro <douglaassgenesis@gmail.com>
2022-03-28 21:54:10 -0300

    aula de picker

--- App.js
+++ App.js
@@ -3,36 +3,45 @@ import {
 View,
 Text,
 StyleSheet,
-ScrollView,
-FlatList
+
 } from 'react-native';
 
-import Pessoas from './src/Pessoas'
+import {Picker} from '@react-native-picker/picker';
+
 
 class App extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			feed: [
-				{id:1, nome: 'Douglas', idade: 33, email: 'douglas@douglas.com'},
-				{id:2, nome: 'Rayene', idade: 28, email: 'rayene@douglas.com'},
-				{id:3, nome: 'Beatriz', idade: 8, email: 'beatriz@douglas.com'},
-				{id:4, nome: 'Bianca', idade: 4, email: 'bianca@douglas.com'},
-				{id:5, nome: 'Ivano', idade: 60, email: 'Ivano@douglas.com'},
+			pizza: 0,
+			pizzas:[
+				{key: 1, nome: 'Calabresa', valor: 35.90},
+				{key: 2, nome: 'Mussarela', valor: 31.90},
+				{key: 3, nome: 'Portuguesa', valor: 33.50},
+				{key: 4, nome: '4 Queijos', valor: 39.70},
+				{key: 5, nome: 'Brigadeiro', valor: 42.15},
 			]
 		}
-
 	}
 
 
 	render(){
+		let pizzasItem = this.state.pizzas.map( (v,k) => {
+			return <Picker.Item key={k} value={k} label={v.nome} />
+		});
 		return (
 			<View style={styles.container}>
-				<FlatList 
-					keyExtractor={(item) => item.id}
-					data={this.state.feed}
-					renderItem={({item}) => <Pessoas data={item} />}
-				/>
+				<Text style={styles.logo}>Menu Pizza</Text>
+
+				<Picker
+					selectedValue={this.state.pizza}
+					onValueChange={ (itemValue, itemKey) => this.setState({pizza: itemValue}) }
+				>
+					{pizzasItem}
+				</Picker>
+
+				<Text style={styles.pizzas}>Você escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
+				<Text style={styles.pizzas}>RS {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
 			</View>
 		);
 	}
@@ -41,8 +50,19 @@ class App extends Component {
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
-	},
+		marginTop: 20,
 
+	},
+	logo:{
+		textAlign: 'center',
+		fontSize: 28,
+		fontWeight: 'bold'
+	},
+	pizzas:{
+		marginTop: 15,
+		fontSize: 23,
+		textAlign: 'center',
+	}
 })
 
 export default App;
