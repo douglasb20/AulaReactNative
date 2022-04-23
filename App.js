da493c15941b3b04835f828c1a2a36ea4ad934a4
Douglas Amaro <douglaassgenesis@gmail.com>
2022-04-23 17:09:43 -0300

    animações

--- App.js
+++ App.js
@@ -7,22 +7,53 @@ StyleSheet,
 Button,
 ActivityIndicator,
 TouchableOpacity,
-Image,
+Animated,
 Keyboard,
 FlatList
 
 } from 'react-native';
 
-import Conversor from './src/Conversor';
-
 export default class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			LarAnimada: new Animated.Value(150),
+			AltAnimada: new Animated.Value(50)
+		}
+
+		Animated.sequence([
+			Animated.timing(
+				this.state.LarAnimada,
+				{
+					toValue: 300,
+					duration: 800,
+					useNativeDriver: false
+				}
+			),
+			Animated.timing(
+				this.state.AltAnimada,
+				{
+					toValue: 150,
+					duration: 800,
+					useNativeDriver: false
+				}
+			)
+		]).start()
+
+	}
 
 	render(){
 
 		return(
 			<View style={styles.container}>
-				<Conversor moedaA="USD" moedaB="BRL"/>
-				<Conversor moedaA="EUR" moedaB="BRL"/>
+			
+				<Animated.View style={{	width:this.state.LarAnimada, 
+								height:this.state.AltAnimada, 
+								backgroundColor:'#4169E1', 
+								justifyContent:'center'}}>
+					<Text style={{color:'#fff', fontSize:22, textAlign:'center'}}>Carregando...</Text>
+				</Animated.View>
+
 			</View>    
 		);
 	}
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-//https://sujeitoprogramador.com/r-api/?api=filmes
-
-const api = axios.create({
-    baseURL: 'https://free.currconv.com/'
-})
-
-export default api;
